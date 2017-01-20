4f3b4c998d1f573c3b44b4f6e74ae60c9eee75b5
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2017-01-20 14:45:32 +0100

    extract input of problem 8 to separate file

--- one_to_fifty.py
+++ one_to_fifty.py
@@ -147,7 +147,7 @@ def problem7(n):
     return primes_found[-1]
 
 def problem8(adjacent_digits=4):
-    number = 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
+    number = int(read_file(filename='problem08_input.txt')[0])
     # number = 12345678901234567890
     number_str = str(number)
     greatest_product = 0
@@ -644,7 +644,7 @@ def main():
     # print_answer(5, 'lowest number which is evenly divisible by all numbers from 1 to 20', problem5(limit=20))
     # print_answer(6, 'the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum', problem6(limit=100))
     # print_answer(7, 'the 10 001st prime number', problem7(10001))
-    # print_answer(8, 'value of the thirteen adjacent digits in the 1000-digit number that have the greatest product', problem8(adjacent_digits=13))
+    print_answer(8, 'value of the thirteen adjacent digits in the 1000-digit number that have the greatest product', problem8(adjacent_digits=13))
     # print_answer(9, 'product of Pythagorean triplet for which a + b + c = 1000', problem9(1000))
     # print_answer(10, 'sum of all primes below two million', problem10(limit=2000000))
     # print_answer(11, 'the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20x20 grid', problem11())
@@ -662,7 +662,7 @@ def main():
     # print_answer(22, 'What is the total of all the name scores in the file?', problem22())
     # print_answer(23, 'Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.', problem23())
     # print_answer(24, 'What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?', problem24())
-    print_answer(25, 'What is the index of the first term in the Fibonacci sequence to contain 1000 digits?', problem25())
+    # print_answer(25, 'What is the index of the first term in the Fibonacci sequence to contain 1000 digits?', problem25())
 
 
 
